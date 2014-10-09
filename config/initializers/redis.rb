fff9fa54bc078efd8632eb11250b62e51acb47b9
vagrant <vagrant@precise32.(none)>
2014-10-09 18:29:47 -0400

    commit2

---
+++ config/initializers/redis.rb
@@ -0,0 +1,2 @@
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://rediscloud:gC2PRxUbp95lQB85@pub-redis-17670.us-east-1-2.2.ec2.garantiadata.com:17670")
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
